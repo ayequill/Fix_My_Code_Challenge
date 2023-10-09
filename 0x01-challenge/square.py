c0c8c349316a88d19b0b3fffc5408e3f0d427b46
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-09 03:07:22 +0000

    fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -26,8 +26,8 @@ class Square:
 
 
 if __name__ == "__main__":
-
-    s = Square(width=12, height=12)
+    """ Create a square instance """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
